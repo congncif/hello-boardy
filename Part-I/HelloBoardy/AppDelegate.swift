d75b003791c680430a1d96c906092339705864c8
NGUYEN CHI CONG <congnc.if@gmail.com>
2021-03-08 09:40:12 +0700

    init project

---
+++ Part-I/HelloBoardy/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  HelloBoardy
+//
+//  Created by NGUYEN CHI CONG on 3/8/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
